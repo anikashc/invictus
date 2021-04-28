f869aab222b0b7bb5315ba630197ab54a5c2ee9d
kamalkarolya <kamalkarolya@gmail.com>
2021-04-28 23:28:12 +0530

    meet the team

--- client/src/Screens/Team.js
+++ client/src/Screens/Team.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import './Team.css';
+import TeamData from './TeamData';
 
-const Team = () => {
+const Team = (props) => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <main className="page-content">
+               {TeamData.map((val)=>{
+
+                    return (
+                        <>
+                         <div className="card" style={{backgroundImage:`url(${val.imgsrc})`}} >
+                    <div className="content">
+                        <h2 className="title">{val.designation} </h2>
+                        <p className="copy"> {val.MebName}</p>
+                        <div>
+                            <a href={val.linkedin} target="_blank"><button className="btn" > <i className="bi bi-linkedin"></i> </button> </a>
+                            <a href={val.instagram} target="_blank">  <button className="btn"  ><i className="bi bi-envelope-fill"></i></button></a>
+                        </div>
+                    </div>
+                </div>
+                        </>
+                    )
+
+
+               })}
+               
+            </main>
+        </>
     )
 }
 
-export default Team
+
+export default Team;
