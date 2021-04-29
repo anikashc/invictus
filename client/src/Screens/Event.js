23f4712a7a7c1a20cfad649a14e6130bf367e289
helewrer3 <helewrer3@gmail.com>
2021-04-29 11:37:18 +0530

    Added basic structure for event page upon which frontend has to be built. The data is being obtained in from EventsData.js in the Meta folder.

--- client/src/Screens/Event.js
+++ client/src/Screens/Event.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import eventsData from '../Meta/EventsData';
 
 const Event = () => {
+    const { id } = useParams()
+    const detail = eventsData[id].detail, date = eventsData[id].schedule.date, time = eventsData[id].schedule.time;
     return (
         <div>
-            
+            <p>Details: {detail}</p>
+            <p>Date: {date}</p>
+            <p>Time: {time}</p>
+            <button>Register</button>
         </div>
     )
 }
