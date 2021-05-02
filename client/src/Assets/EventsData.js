df0ef5dcf88f2ecbc4ca22b21f33605767372f75
helewrer3 <helewrer3@gmail.com>
2021-05-02 10:00:26 +0530

    Updated events data template

--- client/src/Assets/EventsData.js
+++ client/src/Assets/EventsData.js
@@ -1,6 +1,7 @@
 const eventsData = {
     'code' : {
         'summary': 'Coding event',
+        'type': 'coding',
         'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis facere quas quos consequuntur ipsam deleniti quia iure cupiditate! Explicabo hic ex dolorem nihil corporis illo, et neque rem non maxime delectus corrupti minus molestiae nisi deserunt culpa sequi, quia consectetur praesentium obcaecati illum sint voluptatum? Quae eligendi ab molestiae quas!',
         'start': {
             'dateTime': '2021-05-01T21:00:00+05:30',
@@ -13,6 +14,7 @@ const eventsData = {
     },
     'robot' : {
         'summary': 'Robot event',
+        'type': 'robotics',
         'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis facere quas quos consequuntur ipsam deleniti quia iure cupiditate! Explicabo hic ex dolorem nihil corporis illo, et neque rem non maxime delectus corrupti minus molestiae nisi deserunt culpa sequi, quia consectetur praesentium obcaecati illum sint voluptatum? Quae eligendi ab molestiae quas!',
         'start': {
             'dateTime': '2021-05-01T21:00:00+05:30',
