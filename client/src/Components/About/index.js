3e46fa9ed6043b423ce376e6032f69f1a9148923
Shreya Raj <41835298+yay14@users.noreply.github.com>
2021-05-03 23:23:46 +0530

    add backgrounds and titles on landing page

---
+++ client/src/Components/About/index.js
@@ -0,0 +1,75 @@
+import { makeStyles } from '@material-ui/core'
+import React from 'react'
+
+import Box from '@material-ui/core/Box';
+import about_bg from '../../Assets/Backgrounds/about_bg.jpg'
+import about from '../../Assets/Backgrounds/about.png'
+import about_box from '../../Assets/Backgrounds/about_box.png'
+
+const useStyles= makeStyles({
+    root:{
+        background:  `url(${about_bg}) no-repeat`,
+        backgroundSize: '100% 100%',
+        height: '100vh',
+        width: '100%',
+    },
+    aboutContainer:{
+        padding: '150px',    
+        '@media (max-width:599px)': {
+            padding: '50px',
+        }
+    },
+    aboutBox:{ 
+        padding: '45px',
+        background:  `url(${about}) no-repeat`,
+        backgroundSize: '100% 70%',
+        height: '100vh',
+        width: '100%',
+    },
+    aboutContent: {
+        background:  `url(${about_box}) no-repeat`,
+        backgroundSize: '100% 50%',
+        height: '100vh',
+        width: '100%',   
+    },
+    header:{  
+    padding: '40px 0 0 100px',  
+      color: '#FFFFFF', 
+      fontSize: '2.5rem',
+      textAlign: 'left',
+      fontFamily: 'Rajdhani',
+      fontStyle: 'normal',
+      fontWeight: 'bold',
+      },
+      subtext: {
+        padding: '30px', 
+        color: '#000000',  
+        fontSize: '1.5rem',
+        fontFamily: 'Rajdhani',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+      }
+})
+
+const About = () => {
+    const classes= useStyles();
+    return (
+        <div className={classes.root}>
+            <div className={classes.aboutContainer}>
+                <div className={classes.aboutBox}>
+                    <div className={classes.aboutContent}>
+                        
+                    <Box className={classes.header}>ABOUT INVICTUS</Box>
+                    <Box className={classes.subtext}>
+                    INVICTUS is the annual technical fest of DelhiTechnological University, formerly known as Delhi College of Engineering.
+                    Being one of the biggest Tech Fest of North India, it provides a platform for budding Technocrats to explore and put their talent to test when they compete with other prodigies. 
+                    It covers a wide range of events in technical domain with some other miscellaneous events for quizzes and ML competitions to fun games and comedy night.  
+                    </Box>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
---
+++ client/src/Components/Stats/index.js
@@ -0,0 +1,37 @@
+import { makeStyles } from '@material-ui/core'
+import React from 'react'
+
+import stats_bg from '../../Assets/Backgrounds/stats_bg.jpg'
+import Box from '@material-ui/core/Box';
+
+const useStyles= makeStyles({
+    root:{
+        background:  `url(${stats_bg}) no-repeat`,
+        backgroundPosition: 'center',
+        backgroundSize: '100% 100%',
+        height: '100vh',
+        width: '100%',
+    },
+    header:{  
+      paddingTop: '20px',  
+    color: '#FFFFFF', 
+    fontSize: '2.5rem',
+    display: 'flex',
+    justifyContent: 'center',
+    fontFamily: 'Rajdhani',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    }
+})
+
+const Stats = () => {
+    const classes= useStyles();
+    return (
+        <div className={classes.root}>
+            
+          <Box className={classes.header}>PREVIOUS STATS</Box>
+        </div>
+    )
+}
+
+export default Stats
