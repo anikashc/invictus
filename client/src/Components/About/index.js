428d0dc3abda09c8ce027b2fe578ccc3d1c79011
Shreya Raj <41835298+yay14@users.noreply.github.com>
2021-05-04 00:14:26 +0530

    fix: about invictus styles

--- client/src/Components/About/index.js
+++ client/src/Components/About/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 
 import Box from '@material-ui/core/Box';
 import about_bg from '../../Assets/Backgrounds/about_bg.jpg'
-import about from '../../Assets/Backgrounds/about.png'
 import about_box from '../../Assets/Backgrounds/about_box.png'
 
 const useStyles= makeStyles({
@@ -21,16 +20,10 @@ const useStyles= makeStyles({
     },
     aboutBox:{ 
         padding: '45px',
-        background:  `url(${about}) no-repeat`,
+        background:  `url(${about_box}) no-repeat`,
         backgroundSize: '100% 70%',
         height: '100vh',
         width: '100%',
-    },
-    aboutContent: {
-        background:  `url(${about_box}) no-repeat`,
-        backgroundSize: '100% 50%',
-        height: '100vh',
-        width: '100%',   
     },
     header:{  
     padding: '40px 0 0 100px',  
@@ -42,7 +35,7 @@ const useStyles= makeStyles({
       fontWeight: 'bold',
       },
       subtext: {
-        padding: '30px', 
+        padding: '30px 0 0 50px', 
         color: '#000000',  
         fontSize: '1.5rem',
         fontFamily: 'Rajdhani',
@@ -57,15 +50,12 @@ const About = () => {
         <div className={classes.root}>
             <div className={classes.aboutContainer}>
                 <div className={classes.aboutBox}>
-                    <div className={classes.aboutContent}>
-                        
                     <Box className={classes.header}>ABOUT INVICTUS</Box>
                     <Box className={classes.subtext}>
                     INVICTUS is the annual technical fest of DelhiTechnological University, formerly known as Delhi College of Engineering.
                     Being one of the biggest Tech Fest of North India, it provides a platform for budding Technocrats to explore and put their talent to test when they compete with other prodigies. 
                     It covers a wide range of events in technical domain with some other miscellaneous events for quizzes and ML competitions to fun games and comedy night.  
                     </Box>
-                    </div>
                 </div>
             </div>
         </div>
