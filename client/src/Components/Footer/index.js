03caceea4228e5ced589995b67f947887d235a9c
Sakshi <arorasak0@gmail.com>
2021-05-04 13:07:51 +0530

    footer changes

--- client/src/Components/Footer/index.js
+++ client/src/Components/Footer/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import classNames from 'classnames';
 import { Grid, Box } from '@material-ui/core';
 import { useStyles } from './FooterStyles';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
 
 const Footer = () => {
   const classes = useStyles();
@@ -9,16 +12,22 @@ const Footer = () => {
     return (
         <footer> 
         <Grid container className={classes.root}>
-        <Grid item sm={4} className={classes.findus}>
-          <Box className={classes.header}>FOLLOW US ON</Box>
+        <Grid item xs={4} className={classes.findus}>
+          <Box className={classes.header}>FOLLOW US</Box>
           <Box className={classes.socialLinksContainer}>
-            <Box className={classes.iconsContainer}>
-             
+            <Box className={classes.section}>
+             <FacebookIcon fontSize="large"  style = {{color:"#081261"}}/>
+            </Box>
+            <Box className={classes.section} style = {{color:"#081261"}}>
+             <InstagramIcon fontSize="large"/>
+            </Box>
+            <Box className={classes.section} style = {{color:"#081261"}}>
+             <LinkedInIcon fontSize="large"/>
             </Box>
           </Box>
         </Grid>
         
-        <Grid item xs={6} sm={4} className={classes.sitemap}>
+        <Grid item xs={4} className={classes.sitemap}>
           <Box className={classes.header}>SITEMAP</Box>
           <Box className={classNames(classes.subtext, classes.section)}>
             About
@@ -27,16 +36,21 @@ const Footer = () => {
             Events
           </Box>
           <Box className={classNames(classes.subtext, classes.section)}>
+              Team
+          </Box>
+          <Box className={classNames(classes.subtext, classes.section)}>
+            Sponsors
+          </Box>
+          <Box className={classNames(classes.subtext, classes.section)}>
             FAQ
           </Box>
         </Grid>
         <Grid
           item
-          xs={6}
-          sm={4}
+          xs={4}
           className={classes.helpCenterContainer}
         >
-        <Box className={classes.header}>CONTACTS: </Box>
+        <Box className={classes.header}>CONTACTS</Box>
         <Box className={classNames(classes.subtext, classes.help)}>
           +91-9891416666
         </Box>
