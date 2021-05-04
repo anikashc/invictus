01b73d735c4ec90650e224320e6127606cf332de
Ishaan Kamra <ishaan1091@gmail.com>
2021-05-04 18:54:21 +0530

    Restructured  Events Individual page

--- client/src/Components/Event/Schedule.js
+++ client/src/Components/Event/Schedule.js
@@ -3,16 +3,15 @@ import eventsData from "../../Assets/EventsData";
 import Container from "@material-ui/core/Container";
 
 const Schedule = ({ id }) => {
-    const stDate = eventsData[id].start.dateTime, edDate = eventsData[id].end.dateTime;
+    const stDate = eventsData[id].start.dateTime,
+        edDate = eventsData[id].end.dateTime;
     return (
         <section>
-            <Container>
-                <h2 style={{ color: "#3F51B5" }}>Schedule</h2>
-                <p>Start Date: {stDate.substr(0, 10)}</p>
-                <p>Start Time: {stDate.substr(11, 8)}</p>
-                <p>End Date: {edDate.substr(0, 10)}</p>
-                <p>End Time: {edDate.substr(11, 8)}</p>
-            </Container>
+            <h2 style={{ color: "#FFF" }}>Schedule</h2>
+            <p>Start Date: {stDate.substr(0, 10)}</p>
+            <p>Start Time: {stDate.substr(11, 8)}</p>
+            <p>End Date: {edDate.substr(0, 10)}</p>
+            <p>End Time: {edDate.substr(11, 8)}</p>
         </section>
     );
 };
--- client/src/Screens/Event.js
+++ client/src/Screens/Event.js
@@ -1,46 +1,77 @@
-import { React, useState } from 'react';
-import { makeStyles } from '@material-ui/core'
-import Box from '@material-ui/core/Box';
-import event_bg from '../Assets/Backgrounds/event_bg.jpg'
+import { React, useState } from "react";
+import { makeStyles } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import event_bg from "../Assets/Backgrounds/event_bg.jpg";
 import { useParams } from "react-router-dom";
 import Schedule from "../Components/Event/Schedule";
 import Details from "../Components/Event/Details";
+import Image from "../Components/Event/Image";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
+import { Grid } from "@material-ui/core";
 
-
-const useStyles= makeStyles({
-    root:{
-        background:  `url(${event_bg}) no-repeat`,
-        backgroundPosition: 'center',
-        backgroundSize: '100% 100%',
-        height: '100vh',
-        width: '100%',
+const useStyles = makeStyles({
+    root: {
+        background: `url(${event_bg}) no-repeat`,
+        backgroundPosition: "center",
+        backgroundSize: "100% 100%",
+        height: "100vh",
+        width: "100%",
     },
-    header:{  
-        paddingTop: '20px',  
-      color: '#FFFFFF', 
-      fontSize: '2.5rem',
-      display: 'flex',
-      justifyContent: 'center',
-      fontFamily: 'Rajdhani',
-      fontStyle: 'normal',
-      fontWeight: 'bold',
-      }
-})
+    header: {
+        paddingTop: "20px",
+        color: "#FFFFFF",
+        fontSize: "2.5rem",
+        display: "flex",
+        justifyContent: "center",
+        fontFamily: "Rajdhani",
+        fontStyle: "normal",
+        fontWeight: "bold",
+    },
+    eventBody: {
+        margineBottom: "2rem",
+        fontSize: "1.5rem",
+        flexGrow: 1,
+    },
+    button: {},
+    image: {
+        marginTop: "5rem",
+    },
+});
 
 const Event = () => {
     const { id } = useParams();
-    const classes= useStyles();
+    const classes = useStyles();
 
     return (
         <div className={classes.root}>
-            <Details id={id} />
-            <Schedule id={id} />
-            <Container style={{ marginBottom: "2rem" }}>
-                <Button variant="outlined" color="secondary">
-                    Register
-                </Button>
+            <Container className={classes.eventBody}>
+                <Grid container spacing={10}>
+                    <Grid container item xs={7} spacing={3}>
+                        <Details id={id} />
+                    </Grid>
+                    <Grid
+                        container
+                        item
+                        xs={5}
+                        spacing={3}
+                        className={classes.image}
+                    >
+                        <Image id={id} />
+                    </Grid>
+                    <Grid container item xs={12} spacing={3}>
+                        <Schedule id={id} />
+                    </Grid>
+                    <Grid container item xs={12}>
+                        <Button
+                            variant="outlined"
+                            color="secondary"
+                            className={classes.button}
+                        >
+                            Register
+                        </Button>
+                    </Grid>
+                </Grid>
             </Container>
         </div>
     );
