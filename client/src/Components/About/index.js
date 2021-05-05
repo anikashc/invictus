b198def433de1580aff9b1930691580024024d7b
jasmeer-droid <jasmeer57@gmail.com>
2021-05-06 04:06:02 +0530

    responsiveness

--- client/src/Components/About/index.js
+++ client/src/Components/About/index.js
@@ -23,6 +23,9 @@ const useStyles= makeStyles({
         backgroundSize: '100% 70%',
         height: '100vh',
         width: '100%',
+      '@media (max-width:1024px)':{//Laptop View
+        height: '110vh'
+      },
     },
     header:{  
     padding: '50px 0 0 100px',  
@@ -35,43 +38,43 @@ const useStyles= makeStyles({
       lineHeight:'140px',
       position:'static',
       right: '20%',
-      '@media (max-width:768px)':{//Tablet View
-        fontSize:'1.9rem',
+      //MediaQueries
+      '@media (max-width:1440px)':{//L-Laptop View
+        fontSize:'3.6rem',
     },
-    '@media (max-width:599px)':{//Mobile View
-        fontSize:'1.0rem',    
+      '@media (max-width:1024px)':{//Laptop View
+        fontSize: '2.5rem',
+        padding: '10%',
+    },
+    '@media (max-width:768px)':{//Tablet View
+      fontSize: '2rem',
       },
-      '@media (max-width:375px)':{//Mobile View
-        fontSize:'0.7rem',    
-      },
-      '@media (max-width:320px)':{//S-Mobile View
-        fontSize:'6px',
-        lineHeight:'60px',
-        left: '20%',
-        position:'static',
-        overflow: 'hidden',    
+      '@media (max-width:600px)':{//Mobile View 
+        fontSize: '1.3rem',
       },
     },
       subtext: {
         padding: '10px 0 0 50px', 
-        color: '#000000',  
+        color: '#000000', 
         fontSize: '1.8rem',
         fontFamily: 'Rajdhani',
         fontStyle: 'normal',
         fontWeight: 'bold',
-        '@media (max-width:768px)':{//Tablet View
-            fontSize:'1rem',
+        //MediaQueries
+        '@media (max-width:1440px)':{//L-Laptop View
+          fontSize:'2rem',
+          padding: '4%',
+      },
+      '@media (max-width:1024px)':{//Laptop View
+        fontSize: '1.3rem',
+        padding: '3px',
+      },
+      '@media (max-width:768px)':{//Tablet View
+        fontSize:'1.1rem',
         },
-        '@media (max-width:599px)':{//Mobile View
-            fontSize:'0.7rem',    
-          },
-          '@media (max-width:375px)':{//S-Mobile View
-            fontSize:'0.6rem',    
-          },
-          '@media (max-width:320px)':{//Mobile View
-            fontSize:'0.3rem',
-            position: 'relative',    
-          },
+        '@media (max-width:600px)':{//Mobile View 
+          fontSize: '0.84rem'
+        },
         },
 })
 
