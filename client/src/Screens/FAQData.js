c70c2d7d76bee51b63e09a463957861f5069c06f
Anikash Chakraborty <akh.chakraborty11@gmail.com>
2021-05-07 02:39:23 +0530

    FAQ fix

---
+++ client/src/Screens/FAQData.js
@@ -0,0 +1,28 @@
+const FAQData = [
+    {
+      question: 'What if I’ve never been to a hackathon before?',
+      answer: "Perfect! We welcome hackers of all experience levels and backgrounds. We will be providing resources and workshops to help new hackers get started on projects and learn new technologies. There will also be mentors at the event to help guide you. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit perspiciatis in recusandae consequuntur rerum suscipit? Veritatis molestias impedit ea dolorem deleniti asperiores saepe nam laudantium reiciendis fugiat quod quae, minus provident ut, aperiam dicta accusamus eligendi eius sint quis expedita ipsam? Quam veritatis laudantium maiores inventore libero doloribus sit sunt dicta, fugit neque aliquid sed fugiat ipsam soluta non praesentium, blanditiis quod autem hic rerum quae odit ad voluptate! Vitae libero officia minima maiores dolore, sit nam. Cum, aut quis! Officiis, ullam earum expedita suscipit maxime itaque fuga natus? Expedita deleniti temporibus quo sit earum, veniam dolorem qui nisi placeat. ",
+      visible: false,
+      id: 1
+    },
+    {
+      question: 'Can I come with a project pre-built or will I have to build from scratch?',
+      answer: "You will be building your project from scratch. However, feel free to come with ideas of what you want to build!",
+      visible: false,
+      id: 2
+    },
+    {
+      question: 'What events Invictus?',
+      answer: "Invictus is the TechFest of DTU Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, cum? ",
+      visible: false,
+      id: 3
+    },
+    {
+      question: 'What is Invictus?',
+      answer: "Invictus is the TechFest of DTU",
+      visible: false,
+      id: 4
+    }
+  ]
+  
+  export default FAQData;
