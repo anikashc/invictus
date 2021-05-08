98f6e7c669146b5f100ad2c09fde6102530a820c
Anikash Chakraborty <akh.chakraborty11@gmail.com>
2021-05-09 01:10:44 +0530

    404 error removed from netlify and illegal route handle

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { HashRouter as Router, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, useLocation, Switch } from 'react-router-dom';
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Home from './Screens/Home/Home';
@@ -9,7 +9,7 @@ import FAQ from './Screens/FAQ';
 import Sponsor from './Screens/Sponsor/Sponsor';
 import Team from './Screens/Meet_the_team/Team';
 import Schedule from './Screens/Schedule';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles} from '@material-ui/core';
 import MouseParticles from "react-mouse-particles";
 import Loading from './Screens/Loading/Loading'
 import NotFound from './Screens/NotFound'
@@ -61,14 +61,17 @@ function App() {
                 level={6}
               />
             <div>
-              <Route path='/' component={Home} exact />
-              <Route path='/faq' component={FAQ} exact />
-              <Route path='/events' component={Events} exact />
-              <Route path='/sponsors' component={Sponsor} exact />
-              <Route path='/schedule' component={Schedule} />
-              <Route path='/team' component={Team} exact />
-              <Route path='/events/:id' component={Event} />
-              <Route component={NotFound} />
+              <Switch>
+
+                <Route path='/' component={Home} exact />
+                <Route path='/faq' component={FAQ} exact />
+                <Route path='/events' component={Events} exact />
+                <Route path='/sponsors' component={Sponsor} exact />
+                <Route path='/schedule' component={Schedule} />
+                <Route path='/team' component={Team} exact />
+                <Route path='/events/:id' component={Event} />
+                <Route component={NotFound} />
+              </Switch>
             </div>
           </main>
           <Footer />
