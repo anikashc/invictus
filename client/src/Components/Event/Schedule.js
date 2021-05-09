13199c7d3cb44ad8c1ba165f8e456c15f836efab
Ishaan Kamra <ishaan1091@gmail.com>
2021-05-09 12:27:07 +0530

    Made Individual Event page responsive

--- client/src/Components/Event/Schedule.js
+++ client/src/Components/Event/Schedule.js
@@ -6,12 +6,20 @@ const Schedule = ({ id }) => {
     const stDate = eventsData[id].start.dateTime,
         edDate = eventsData[id].end.dateTime;
     return (
-        <section style={{ color: "#FFF" }}>
+        <section style={{ color: "#FFF", textAlign: "left" }}>
             <h2 style={{ color: "#EE81B2" }}>Schedule</h2>
-            <p>Start Date: {stDate.substr(0, 10)}</p>
-            <p>Start Time: {stDate.substr(11, 8)}</p>
-            <p>End Date: {edDate.substr(0, 10)}</p>
-            <p>End Time: {edDate.substr(11, 8)}</p>
+            <p style={{ marginTop: "1rem", lineHeight: "1.5rem" }}>
+                Start Date: {stDate.substr(0, 10)}
+            </p>
+            <p style={{ lineHeight: "1.5rem" }}>
+                Start Time: {stDate.substr(11, 8)}
+            </p>
+            <p style={{ lineHeight: "1.5rem" }}>
+                End Date: {edDate.substr(0, 10)}
+            </p>
+            <p style={{ lineHeight: "1.5rem" }}>
+                End Time: {edDate.substr(11, 8)}
+            </p>
         </section>
     );
 };
--- client/src/Screens/NotFound.js
+++ client/src/Screens/NotFound.js
@@ -1,13 +1,8 @@
-import { React, useState } from "react";
-import { makeStyles } from "@material-ui/core";
-import Box from "@material-ui/core/Box";
-import { Link, useParams } from "react-router-dom";
-import Schedule from "../Components/Event/Schedule";
-import Details from "../Components/Event/Details";
-import Image from "../Components/Event/Image";
+import { React } from "react";
 import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core";
+import Link from "@material-ui/core/Link";
 import { Grid } from "@material-ui/core";
-import { AppBar, Toolbar, Button, IconButton, Fade } from "@material-ui/core";
 
 const useStyles = makeStyles({
     root: {
@@ -16,8 +11,10 @@ const useStyles = makeStyles({
             "-webkit-linear-gradient(to top, #3c1053, #ad5389)" /* Chrome 10-25, Safari 5.1-6 */,
         background: "linear-gradient(to top,#642A7F, #1B1430)",
         /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-        height: "100vh",
+        height: "100%",
         width: "100%",
+        padding: "3rem 0",
+        fontSize: "2rem",
     },
     header: {
         paddingTop: "20px",
@@ -30,38 +27,37 @@ const useStyles = makeStyles({
         fontWeight: "bold",
     },
     eventBody: {
-        margineBottom: "2rem",
-        fontSize: "1.5rem",
-        // flexGrow: 1,
-    },
-    register: {
-        background: "#D66496",
-        fontFamily: "Rajdhani",
-        color: "white",
-        fontWeight: "bold",
-        padding: "5px 25px 5px 25px",
-        "&:hover": {
-            backgroundColor: "#DB4789",
+        margin: "65% 0",
+        fontSize: "2rem",
+        color: "#FFF",
+        "@media (min-width:600px)": {
+            //mobile view
+            margin: "20rem 0",
         },
-        fontSize: "1.5rem",
-        margin: "-15px",
-    },
-    image: {
-        marginTop: "5rem",
     },
 });
 
-const Event = () => {
-
+const NotFound = () => {
     const classes = useStyles();
-
     return (
         <div className={classes.root}>
-            <Container className={classes.header}>
-                404- Page Not found. Return to <Link to='/'> Home</Link>
+            <Container className={classes.eventBody}>
+                <Grid container align="center" justify="center">
+                    <Grid item>
+                        <h3>
+                            404- Page Not found. Return to{" "}
+                            <Link
+                                to="/"
+                                style={{ color: "#EE81B2", cursor: "pointer" }}
+                            >
+                                Home
+                            </Link>
+                        </h3>
+                    </Grid>
+                </Grid>
             </Container>
         </div>
     );
 };
 
-export default Event;
+export default NotFound;
